0a46fe62a2262f08e2d0a4e271374e0c3d7a4055
boyninja1555 <flappyfloorg@gmail.com>
2026-05-09 00:37:50 -0500

    Pay UI

--- web/src/api.ts
+++ web/src/api.ts
@@ -1,6 +1,8 @@
+import { getNumber } from "./numberlib"
+
 const URL = "https://fbxapi.flappygrant.com"
 
-export async function getBalance(number: string) {
+export async function getBalance(number: string): Promise<number | string> {
     try {
         const req = await fetch(`${URL}/balance`, {
             method: "POST",
@@ -9,14 +11,29 @@ export async function getBalance(number: string) {
         })
 
         const res = await req.json()
-        if (res.status != 200) {
-            console.error(res.message)
-            return -1
-        }
-
+        if (res.status != 200) return res.message
         return res.data as number
     } catch (error) {
-        console.error(error)
-        return -1
+        return (error instanceof Error ? error.message : String(error))
+    }
+}
+
+export async function payUser(number: string, amount: number): Promise<string | undefined> {
+    try {
+        const req = await fetch(`${URL}/pay`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json", },
+            body: JSON.stringify({
+                number,
+                amount,
+                source: getNumber(),
+            }),
+        })
+
+        const res = await req.json()
+        if (res.status != 200) return res.message
+        return
+    } catch (error) {
+        return (error instanceof Error ? error.message : String(error))
     }
 }
--- web/src/reactivity.tsx
+++ web/src/reactivity.tsx
@@ -1,23 +1,36 @@
-import { HTMLAttributes, render } from "preact"
+import { type JSX, render } from "preact"
 import { useEffect, useState } from "preact/hooks"
-import { getBalance } from "./api"
+import { getBalance, payUser } from "./api"
 import { getNumber, setNumber } from "./numberlib"
 
 // Components
 
-function Button(props: HTMLAttributes<HTMLButtonElement>) {
-    const className = `py-1 px-2 bg-accent hover:bg-accent/90 active:bg-accent/75 text-white rounded-md shadow-md ${props.className}`
+function Button(props: JSX.IntrinsicElements["button"]) {
+    const className = `py-2 px-3 bg-accent hover:bg-accent/90 active:bg-accent/75 outline-none text-white rounded-md shadow-md ${props.className}`
     props.className = undefined
     return <button {...props} className={className} />
+}
+
+function Input(props: JSX.IntrinsicElements["input"]) {
+    const className = `py-2 px-3 bg-secondary outline-none text-tsecondary rounded-md shadow-md ${props.className}`
+    props.className = undefined
+    return <input {...props} className={className} />
 }
 
 // Pages
 
 function PageAccount() {
-    const [balance, setBalanceCache] = useState(-1)
+    const [balance, setBalanceCache] = useState(0)
+    const [error, setError] = useState<string | null>(null)
 
     async function refreshBalanceCache() {
-        setBalanceCache(await getBalance(getNumber()!))
+        const newCache = await getBalance(getNumber()!)
+        if (typeof newCache === "string") {
+            setError(newCache)
+            return
+        }
+
+        setBalanceCache(newCache)
     }
 
     async function promptNumber() {
@@ -27,8 +40,9 @@ function PageAccount() {
             return
         }
 
+        setError(null)
         setNumber(number)
-        setBalanceCache(await getBalance(getNumber()!))
+        refreshBalanceCache()
     }
 
     useEffect(() => {
@@ -37,7 +51,9 @@ function PageAccount() {
 
     return (
         <div className="flex flex-col gap-2 top-1/2 left-1/2 -translate-1/2 absolute">
-            {(balance !== -1) && (
+            {error ? (
+                <span class="text-center text-3xl text-red-500">{error}</span>
+            ) : (
                 <span className="text-center text-3xl">${balance} <abbr title="Floorbux">FBX</abbr></span>
             )}
 
@@ -48,4 +64,53 @@ function PageAccount() {
     )
 }
 
-render(<PageAccount />, document.getElementById("account")!)
+function PagePay() {
+    const [error, setError] = useState<string | null>(null)
+    const [number, setNumber] = useState("")
+    const [amount, setAmount] = useState(1)
+
+    async function onSubmit(event: SubmitEvent) {
+        event.preventDefault()
+        setError(null)
+
+        const error = await payUser(number, amount)
+        if (error) {
+            setError(error)
+            return
+        }
+
+        setNumber("")
+        setAmount(1)
+    }
+
+    return (
+        <form onSubmit={onSubmit} className="flex flex-col gap-2 top-1/2 left-1/2 -translate-1/2 absolute">
+            <h1 className="text-center text-2xl">Pay a user</h1>
+
+            <Input type="text"
+                placeholder="Recipient's number..."
+                value={number}
+                onInput={e => setNumber(e.currentTarget.value)}
+                required />
+
+            <Input type="number"
+                placeholder="Amount..."
+                value={amount}
+                onInput={e => setAmount(parseInt(e.currentTarget.value, 10))}
+                min={1}
+                required />
+
+            <Button>Pay</Button>
+            {error && <span class="text-sm text-red-500">{error}</span>}
+        </form>
+    )
+}
+
+function rendering() {
+    render(<PageAccount />, document.getElementById("account")!)
+    render(<PagePay />, document.getElementById("pay")!)
+    render(<PageAccount />, document.getElementById("charge")!)
+}
+
+window.addEventListener("hashchange", rendering)
+rendering()
