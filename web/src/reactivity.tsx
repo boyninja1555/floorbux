60079db26b7a98e3789301583f58d5bec91b3333
boyninja1555 <flappyfloorg@gmail.com>
2026-05-08 23:13:01 -0500

    Better balance display

--- web/src/reactivity.tsx
+++ web/src/reactivity.tsx
@@ -1,32 +1,50 @@
-import { render } from "preact"
+import { HTMLAttributes, render } from "preact"
 import { useEffect, useState } from "preact/hooks"
 import { getBalance } from "./api"
 import { getNumber, setNumber } from "./numberlib"
 
+// Components
+
+function Button(props: HTMLAttributes<HTMLButtonElement>) {
+    const className = `py-1 px-2 bg-accent hover:bg-accent/90 active:bg-accent/75 text-white rounded-md shadow-md ${props.className}`
+    props.className = undefined
+    return <button {...props} className={className} />
+}
+
+// Pages
+
 function PageAccount() {
-    const [balance, setBalanceCache] = useState(0)
+    const [balance, setBalanceCache] = useState(-1)
+
+    async function refreshBalanceCache() {
+        setBalanceCache(await getBalance(getNumber()!))
+    }
+
+    async function promptNumber() {
+        const number = prompt("Enter your number...")
+        if (!number) {
+            promptNumber()
+            return
+        }
+
+        setNumber(number)
+        setBalanceCache(await getBalance(getNumber()!))
+    }
 
     useEffect(() => {
-        function promptNumber() {
-            const number = prompt("Enter your number...")
-            if (!number) {
-                promptNumber()
-                return
-            }
-
-            setNumber(number)
-        }
-
-        async function load() {
-            if (!getNumber()) promptNumber()
-            setBalanceCache(await getBalance(getNumber()!))
-        }
-
-        load()
+        refreshBalanceCache()
     }, [])
 
     return (
-        <p>{balance}</p>
+        <div className="flex flex-col gap-2 top-1/2 left-1/2 -translate-1/2 absolute">
+            {(balance !== -1) && (
+                <span className="text-center text-3xl">${balance} <abbr title="Floorbux">FBX</abbr></span>
+            )}
+
+            <Button onClick={promptNumber}>
+                {balance === -1 ? "Choose Number" : "Switch Number"}
+            </Button>
+        </div>
     )
 }
 
